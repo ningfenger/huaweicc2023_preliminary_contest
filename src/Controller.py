28872bd96d62c318a739f60900a9a2de8e973213
littleHuang <ysh626@foxmail.com>
2023-03-17 22:24:26 +0800

    框架v2

---
+++ src/Controller.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+class Controller:
+    def __init__(self, robots):
+        self.robots = robots
+    def move(self):
+        pass
--- src/robot.py
+++ src/robot.py
@@ -8,11 +8,12 @@ class Robot:
         self.loc = loc
         self.speed = 0.0  # 线速度
         self.turn = 0.0  # 角速度
-        self.workstation_ID = 0  # 所处工作台ID
+        self.workstation_ID = -1  # 所处工作台ID代表没有
         self.item_type = 0  # 携带物品类型
         self.time_value = 0.0  # 时间价值系数
         self.clash_value = 0.0  # 碰撞价值系数
         self.status: int = 0  # 0 空闲, 1 购买途中, 2 等待购买, 3 出售途中, 4 等待出售
+        self.target = -1 # 当前机器人的目标控制台 -1代表无目标
 
     def update(self, str):
         # 根据判题器的输入更新机器人状态, 记得更新status
