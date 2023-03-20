303ddbbbec3580ec54bceb3c27863e66a16884b7
littleHuang <ysh626@foxmail.com>
2023-03-20 12:21:56 +0800

    改了点参数再训

--- train.py
+++ train.py
@@ -6,22 +6,32 @@ import random
 from sys import platform
 
 Robot = 'Robot.exe'
-if platform=='linux':
+if platform == 'linux':
     Robot = './Robot'
-# 定义目标函数
-def target_function(x1, x2, x3, x4, x5, x6, x7, x8):
-    # 在这里写入你的目标函数代码
-    maps = list(range(1, 5))
-    total_score = 0
+
+# 转化归一化的值
+
+
+def get_final_value(x1, x2, x3, x4, x5, x6, x7, x8):
     DIS_1 = 0.3+x1  # 开始刹车的距离 0.3~1.3
     VELO_1 = 0.1+x2  # 刹车时的速度 0.1~1.1
-    MOVE_SPEED = 1 /(2+3*x3) * 50  # 估算移动时间 2~5
+    MOVE_SPEED = 1 / (2+3*x3) * 50  # 估算移动时间 2~5
     MAX_WAIT = (2+8*x4) * 50  # 最大等待时间 2~10
     SELL_WEIGHT = 1+x5  # 优先卖给格子被部分占用的 1~2
     # 人工势场常数
     ETA = 100+400*x6  # 调整斥力大小的常数 100~500
     GAMMA = 5+15*x7  # 调整吸引力大小的常数 5~20
     RADIUS = 2+8*x8  # 定义斥力半径范围 2-10
+    return DIS_1, VELO_1, MOVE_SPEED, MAX_WAIT, SELL_WEIGHT, ETA, GAMMA, RADIUS
+# 定义目标函数
+
+
+def target_function(x1, x2, x3, x4, x5, x6, x7, x8):
+    # 在这里写入你的目标函数代码
+    maps = list(range(1, 5))
+    total_score = 0
+    DIS_1, VELO_1, MOVE_SPEED, MAX_WAIT, SELL_WEIGHT, ETA, GAMMA, RADIUS = get_final_value(
+        x1, x2, x3, x4, x5, x6, x7, x8)
     print(f'参数: --dis_1 {DIS_1} --velo_1 {VELO_1} --move_speed {MOVE_SPEED} --max_wait {MAX_WAIT} \
         --sell_weight {SELL_WEIGHT} --eta {ETA} --gamma {GAMMA} --radius {RADIUS}')
     for map in maps:
@@ -33,25 +43,27 @@ def target_function(x1, x2, x3, x4, x5, x6, x7, x8):
         total_score += score
         print(f'地图{map}得分为: {score}')
     print(f'总得分为: {total_score}')
-    return total_score
+    return total_score/10**6
 
 
-def train(num_iterations=100, learning_rate = 0.1):
-    print(f'num_iterations:{num_iterations}, learning_rate:{learning_rate}')
+def train(num_iterations=100):
     # 定义可行解的范围（最小值和最大值）
     boundaries = [(0, 1.0)]*8
     # 定义初始参数
-    params = [random.uniform(boundaries[i][0], boundaries[i][1]) for i in range(len(boundaries))]
+    params = [random.uniform(boundaries[i][0], boundaries[i][1])
+              for i in range(len(boundaries))]
     # 开始迭代
     bast_value = 0
     bast_param = []
+    delta = 0.05
+    learning_rate = delta*0.5
+    print(f'num_iterations:{num_iterations}, learning_rate:{learning_rate}')
     for i in range(num_iterations):
-        print(F"\n\n第{i+1}轮训练"+'.'*50)
+        print(F"\n\n第{i+1}轮训练"+'.'*100)
         # 计算梯度
         grad = []
         old_value = target_function(*params)
         for j in range(len(params)):
-            delta = 0.1
             new_params = params.copy()
             new_params[j] += delta
             new_value = target_function(*new_params)
@@ -60,21 +72,21 @@ def train(num_iterations=100, learning_rate = 0.1):
                 bast_param = new_params.copy()
             derivative = (new_value - old_value) / delta
             grad.append(derivative)
-        
+
         # 更新参数
         for j in range(len(params)):
             params[j] += learning_rate * grad[j]
             # 确保参数在可行解范围内
             params[j] = max(params[j], boundaries[j][0])
             params[j] = min(params[j], boundaries[j][1])
-        print("截至目前最优解为：", bast_param)
+        print("截至目前最优解为：", get_final_value(*bast_param))
         print("截至目前最优值为：", bast_value)
-    print("最优解为：", bast_param)
+    print("最优解为：", get_final_value(*bast_param))
     print("最优值为：", bast_value)
     # 定义初始参数
     # initial_params = [random.uniform(
-        # boundaries[i][0], boundaries[i][1]) for i in range(len(boundaries))]
-    
+    # boundaries[i][0], boundaries[i][1]) for i in range(len(boundaries))]
+
     # current_value = target_function(*initial_params)
 
     # 尝试在每个方向上增加/减少参数，并找到最好的参数组合
@@ -105,8 +117,8 @@ def train(num_iterations=100, learning_rate = 0.1):
     #                 best_value = new_value
     #                 best_params = new_params
 
-        # 更新参数
-        # initial_params = best_params
+    # 更新参数
+    # initial_params = best_params
 
     # print("最优解为：", initial_params)
     # print("最优值为：", target_function(*initial_params))
